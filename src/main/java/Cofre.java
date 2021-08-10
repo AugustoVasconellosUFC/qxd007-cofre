0ce58c2d8bc07df485da20f5631be176b081729a
Augusto Vasconcelos <augustovnd@gmail.com>
2021-08-10 01:44:45 -0300

    *entrega

--- src/main/java/Cofre.java
+++ src/main/java/Cofre.java
@@ -1,46 +1,102 @@
+import java.util.*;
+
 public class Cofre {
 
+    private final Stack<Item> itens= new Stack<>();
+
+    private final Stack<Moeda> moedas= new Stack<>();
+
+    private int volume=0;
+
+    private final int volMaximo;
+
+    private int volRestante;
+
+    private boolean quebrado=false;
+
     public Cofre(int volumeMaximo) {
+        volMaximo=volumeMaximo;
+        volRestante=volumeMaximo;
     }
 
     public int getVolume() {
-        return -1;
+        return volume;
     }
 
     public int getVolumeMaximo() {
-        return -1;
+        return volMaximo;
     }
 
     public int getVolumeRestante() {
-        return -1;
+        return volRestante;
     }
 
     public boolean add(Item item) {
-        return false;
+        if(volRestante-item.getVolume()>=0&&!quebrado){
+            volRestante-=item.getVolume();
+            volume+=item.getVolume();
+            itens.push(item);
+            return true;
+        }
+        else
+            return false;
     }
 
     public boolean add(Moeda moeda) {
-        return false;
+        if(volRestante-moeda.getVolume()>=0&&!quebrado){
+            volRestante-=moeda.getVolume();
+            volume+=moeda.getVolume();
+            moedas.push(moeda);
+            return true;
+        }
+        else
+            return false;
     }
 
     public String obterItens() {
-        return "vazio";
+        if(quebrado){
+            return getItens();
+        }
+        else
+            return null;
     }
 
     private String getItens() {
-        return null;
+        StringBuilder s= new StringBuilder();
+        if(itens.empty())
+            s.append("vazio");
+        while(!itens.empty()) {
+            s.insert(0, itens.peek().getDescricao());
+            itens.pop();
+            if(!itens.empty())
+                s.insert(0, ", ");
+        }
+        return s.toString();
     }
 
     public double obterMoedas() {
-        return -1;
+        if(quebrado){
+            double valor=0;
+            while(!moedas.empty()) {
+                valor+=moedas.peek().getValor();
+                moedas.pop();
+            }
+            return valor;
+        }
+        else
+            return -1;
     }
     
     public boolean taInteiro() {
-        return false;
+        return !quebrado;
     }
 
     public boolean quebrar() {
-
-        return false;
+        if(!quebrado){
+            quebrado=true;
+            return true;
+        }
+        else
+            return false;
     }
 }
